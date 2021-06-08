6cf523d8cc5e7d869315743e4782b2e0fd51e36b
Isabell Carrascal <mi.carrascal@uniandes.edu.co>
2021-06-08 07:59:23 -0500

    Gimnasio updated

--- src/persistencia/SQLGimnasio.java
+++ src/persistencia/SQLGimnasio.java
@@ -2,4 +2,38 @@ package uniandes.isis2304.parranderos.persistencia;
 
 public class SQLGimnasio {
 
+public long adicionarGimnasio (PersistenceManager pm, String pId ) 
+	{
+        Query q = pm.newQuery( SQL, "INSERT INTO " + pp.GIMNASIO + "(id,) values (?, )");
+        q.setParameters( pId);
+        return (long) q.executeUnique();            
+	}
+	
+	/**
+	 * Elimina un salon de la base de datos
+	 * @param pm
+	 * @param idSalon
+	 * @return
+	 */
+ 	public long eliminarPiscinaPorId (PersistenceManager pm, String idInternet)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.PISCINA + " WHERE id = ?");
+        q.setParameters(idInternet);
+        return (long) q.executeUnique();            
+	}
+ 	
+ /**
+  * Retorna una lista con todos los salones del hotel
+  * @param pm
+  * @return
+  */
+	public List<Internet> darPiscina (PersistenceManager pm)
+	{
+		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.PISCINA);
+		q.setResultClass(Internet.class);
+		return (List<Internet>) q.executeList();
+	}
+
+}
+
 }
